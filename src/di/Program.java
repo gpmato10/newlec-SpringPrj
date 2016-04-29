fab0bd554579de13a8e7229c23bbac4e4a0dae62
dw <gpmato10@gmail.com>
2016-04-29 21:49:28 +0900

    1.3 스프링 설치와 DI를 활용하는 예제

--- src/di/Program.java
+++ src/di/Program.java
@@ -1,14 +1,20 @@
 package di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by dw on 2016. 4. 29..
  */
 public class Program {
     public static void main(String[] args) {
-        Record record = new NewlecRecord();
-        RecordView view = new NewlecRecordView();
+//        Record record = new NewlecRecord();
+//        RecordView view = new NewlecRecordView();
+//
+//        view.setRecord(record);
 
-        view.setRecord(record);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+        RecordView view = (RecordView) ctx.getBean("view");
 
         view.input();
         view.print();
